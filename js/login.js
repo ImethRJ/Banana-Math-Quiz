be84e5ff551d4fec6fa790b3b0c3fa270df86182
ImethRJ <136222731+ImethRJ@users.noreply.github.com>
2025-03-14 08:07:45 +0530

    login page finished with added styles and javascript
    login page finished with added styles and javascript

---
+++ js/login.js
@@ -0,0 +1,20 @@
+        function login() {
+            const username = document.getElementById("username").value;
+            if (username) {
+                localStorage.setItem("currentUser", username);
+                window.location.href = "levels.html";
+            } else {
+                alert("Please enter a username to login.");
+            }
+        }
+
+        function signup() {
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+            if (username && password) {
+                localStorage.setItem(username, password);
+                alert("Signup successful! You can now login.");
+            } else {
+                alert("Please enter a username and password to sign up.");
+            }
+        }
